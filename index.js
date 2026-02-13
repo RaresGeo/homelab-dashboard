8ba9739da60b175b6c78056176185173b4468a38
Daniel Arch <raresgeo9@gmail.com>
2026-02-13 13:25:11 +0200

    feat: dynamic creation of links

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Loaded js...")
+const services = {
+  plex: 32400,
+  transmission: 9091,
+  radarr: 7878,
+  jackett: 9117,
+  bazarr: 6767
+};
+
+const hostname = window.location.hostname;  // nixos.local or nixos.xxx.ts.net
+const baseUrl = `http://${hostname}`;
+const links = document.getElementById('services');
+
+Object.entries(services).forEach(([name]) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.href = `${baseUrl}:${services[name]}`;  // Dynamic port
+  a.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+  li.appendChild(a);
+  links.appendChild(li);
+});
