2010fb928623b75069eee32b7fa934a5020780cc
Daniel Arch <raresgeo9@gmail.com>
2026-03-13 18:15:48 +0200

    fix: use full hostname

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ const services = [
 ];
 
 const hostname = window.location.hostname; // kryllix.dedyn.io or nixos.local etc.
-const baseDomain = hostname.split('.').slice(1).join('.'); // strips the subdomain if any
 const links = document.getElementById('services');
 
 services.forEach(name => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.href = `https://${name}.${baseDomain}`;
+    a.href = `https://${name}.${hostname}`;
     a.textContent = name.charAt(0).toUpperCase() + name.slice(1);
     li.appendChild(a);
     links.appendChild(li);
