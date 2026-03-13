f5c0a14b44c93c831913de5c5a5c8be5fe1a4807
Daniel Arch <raresgeo9@gmail.com>
2026-03-13 18:13:30 +0200

    feat: use new domain

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const services = {
-    plex: "32400/web",
-    transmission: 9091,
-    radarr: 7878,
-    jackett: 9117,
-    bazarr: 6767,
-    minIO: 9001,
-    finances: "/finance/",
-    copyparty: 3923
-};
+const services = [
+    "plex",
+    "transmission",
+    "radarr",
+    "jackett",
+    "bazarr",
+    "minio",
+    "finance",
+    "copyparty"
+];
 
-const hostname = window.location.hostname;  // nixos.local or nixos.xxx.ts.net
-const baseUrl = `http://${hostname}`;
+const hostname = window.location.hostname; // kryllix.dedyn.io or nixos.local etc.
+const baseDomain = hostname.split('.').slice(1).join('.'); // strips the subdomain if any
 const links = document.getElementById('services');
 
-Object.entries(services).forEach(([name]) => {
+services.forEach(name => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.href = `${baseUrl}:${services[name]}`;  // Dynamic port
+    a.href = `https://${name}.${baseDomain}`;
     a.textContent = name.charAt(0).toUpperCase() + name.slice(1);
     li.appendChild(a);
     links.appendChild(li);
