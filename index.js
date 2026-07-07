75e3d5c4f0e5e2f653f0d49bc809827c96ace7e7
Daniel Arch <raresgeo9@gmail.com>
2026-07-07 11:57:01 +0300

    feat: overhaul styles and add public services

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const services = [
+const publicServices = [
+    "tierlists"
+];
+
+const privateServices = [
     "plex",
-    "jellyfin",
     "transmission",
     "radarr",
     "jackett",
@@ -8,17 +11,22 @@ const services = [
     "minio",
     "finance",
     "copyparty",
-    "tierlists"
 ];
 
-const hostname = window.location.hostname; // kryllix.dedyn.io or nixos.local etc.
-const links = document.getElementById('services');
+const hostname = window.location.hostname;
 
-services.forEach(name => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.href = `https://${name}.${hostname}`;
-    a.textContent = name.charAt(0).toUpperCase() + name.slice(1);
-    li.appendChild(a);
-    links.appendChild(li);
-});
+function appendServices(servicesArr, appendToId) {
+    const links = document.getElementById(appendToId);
+
+    servicesArr.forEach(name => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = `https://${name}.${hostname}`;
+        a.textContent = name.charAt(0).toUpperCase() + name.slice(1);
+        li.appendChild(a);
+        links.appendChild(li);
+    });
+}
+
+appendServices(privateServices, 'private-services')
+appendServices(publicServices, 'public-services')
